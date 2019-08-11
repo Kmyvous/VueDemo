ca7850e0c6b5f7af14e47699acc7c89ca1cfab90
kmyvous <939002750@qq.com>
2019-08-11 09:30:34 +0800

    Header区完成

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 入口文件
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App.vue'
 
-// import MintUI from 'mint-ui'
-// import 'mint-ui/lib/style.css'
-// Vue.use(MintUI)
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 
+// 按需引入 mint-ui
 import { Header } from 'mint-ui'
 import 'mint-ui/lib/style.css'
 Vue.component(Header.name, Header)
 
-Vue.config.productionTip = false
-/* eslint-disable no-new */
+// 按需引入 MUI
+import './lib/dist/css/mui.min.css'
+import './lib/dist/css/icons-extra.css'
+import './lib/dist/fonts/mui-icons-extra.ttf'
+
+// 导入自定义的 router 模块
+import router from './router.js'
+
+
 new Vue({
   el: '#app',
+  render: c => c(App),
   router,
-  components: { App },
-  template: '<App/>'
 })
